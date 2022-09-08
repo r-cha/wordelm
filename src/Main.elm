44ffc3cb3a1d34a517a3bcdda7ebd05341697d76
r-cha <robert@akchandler.com>
2022-09-07 17:21:57 -0700

    idk fix some stuff

--- src/Main.elm
+++ src/Main.elm
@@ -8,9 +8,10 @@ import Html exposing (..)
 import Html.Attributes as Attr
 import Json.Decode as Decode
 
-import Random.Extra as RE
+
 
 -- MAIN
+
 
 main : Program () Model Msg
 main =
@@ -22,40 +23,53 @@ main =
         }
 
 
+
 -- MODEL
 
-type alias Model = 
+
+type alias Model =
     { current : List Char
     , guesses : List String
     , scores : List (List Mark)
     , answer : String
+
     -- TODO: Use a single 5x6 array
     -- TODO: Add an error message field
     }
 
-type alias Mark = Int  -- 0=no, 1=yesish, 2=yes
+
+type alias Mark =
+    Int
+
+
+
+-- 0=no, 1=yesish, 2=yes
 
 
 emptyModel : Model
-emptyModel = 
+emptyModel =
     { current = []
     , guesses = []
     , scores = []
-    , answer = "OPALS"  -- TODO: RE.choice ["WORKS", "FLAKE", "OPALS"]
+    , answer = "OPALS" -- TODO: RE.choice ["WORKS", "FLAKE", "OPALS"]
     }
+
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Maybe.withDefault emptyModel Nothing
-  , Cmd.none
-  )
+    ( Maybe.withDefault emptyModel Nothing
+    , Cmd.none
+    )
+
 
 
 -- UPDATE
 
+
 type Msg
-  = Character Char
-  | Control String
+    = Character Char
+    | Control String
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -68,6 +82,7 @@ update msg model =
                 ( { model | current = char :: model.current }
                 , Cmd.none
                 )
+
             else
                 ( model, Cmd.none )
 
@@ -75,74 +90,134 @@ update msg model =
             -- Process a submission.
             -- This should only do anything if there are 5 letters available.
             if List.length model.current == 5 then
-                let guess = String.reverse (String.fromList model.current)
+                let
+                    guess =
+                        String.reverse (String.fromList model.current)
                 in
-                ( { model 
+                ( { model
                     | current = []
                     , guesses = guess :: model.guesses
-                    , scores = (checkGuess guess model.answer) :: model.scores
+                    , scores = checkGuess guess model.answer :: model.scores
                   }
                 , Cmd.none
                 )
+
             else
                 ( model, Cmd.none )
 
         Control "Backspace" ->
             -- Remove the last letter
             let
-                old = 
+                old =
                     case model.current of
-                        _ :: tail -> tail
-                        [] -> []
-            in 
+                        _ :: tail ->
+                            tail
+
+                        [] ->
+                            []
+            in
             ( { model | current = old }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
 
 
-checkChars : String -> (Char, Char) -> Mark
-checkChars fullAnswer (guess, answer) =
+checkChars : String -> ( Char, Char ) -> Mark
+checkChars fullAnswer ( guess, answer ) =
     if guess == answer then
         2
+
     else if String.contains (String.fromChar guess) fullAnswer then
         1
+
     else
         0
 
+
 checkGuess : String -> String -> List Mark
 checkGuess guess answer =
-    List.map 
-      (checkChars answer)
-      ( List.map2 
-          Tuple.pair
-          (String.toList guess) (String.toList answer)
-      )
+    List.map
+        (checkChars answer)
+        (List.map2
+            Tuple.pair
+            (String.toList guess)
+            (String.toList answer)
+        )
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
-view model = 
+view model =
     div
-        [ Attr.class "wordelm-app"]
-        [ h1 [] [ text "Wordelm"]
-        , viewGuesses model.guesses
-        , viewCurrent model.current
+        [ Attr.class "wordelm-app" ]
+        [ heading
+        , viewBoard model
+        , viewKeys
         ]
 
+
+heading : Html Msg
+heading =
+    h1 [ Attr.class "heading" ] [ text "Wordelm" ]
+
+
+viewBoard : Model -> Html Msg
+viewBoard model =
+    div
+        [ Attr.class "board" ]
+        [ viewRow model 0
+        , viewRow model 1
+        , viewRow model 2
+        , viewRow model 3
+        , viewRow model 4
+        , viewRow model 5
+        ]
+
+
+viewRow : Model -> Int -> Html Msg
+viewRow model index =
+    div
+        [ Attr.class "row" ]
+        [ viewTile 'R' 0
+        , viewTile 'O' 1
+        , viewTile 'C' 2
+        ]
+
+
+viewTile : Char -> Mark -> Html Msg
+viewTile letter mark =
+    div
+        [ Attr.class "tile" ]
+        [ Html.text (String.fromChar letter) ]
+
+
+viewKeys : Html Msg
+viewKeys =
+    div
+        [ Attr.class "keyboard" ]
+        [ Html.p [ Attr.class "keyboard-row" ] [ Html.text "QWERTYUIOP" ]
+        , Html.p [ Attr.class "keyboard-row" ] [ Html.text "ASDFGHJKL" ]
+        , Html.p [ Attr.class "keyboard-row" ] [ Html.text " ZXCVBNM<" ]
+        ]
+
+
 viewCurrent : List Char -> Html Msg
-viewCurrent current = 
+viewCurrent current =
     Html.text (String.reverse (String.fromList current))
+
 
 viewGuesses : List String -> Html Msg
 viewGuesses guesses =
     Html.ul
-        [ Attr.class "guess-list"]
+        [ Attr.class "guess-list" ]
         (List.reverse (List.map viewGuess guesses))
 
+
 viewGuess : String -> Html Msg
-viewGuess guess = 
+viewGuess guess =
     -- TODO: Visualize score of guess (model.scores)
     Html.li
         [ Attr.class "guess" ]
@@ -152,18 +227,22 @@ viewGuess guess =
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     onKeyDown keyDecoder
 
+
 keyDecoder : Decode.Decoder Msg
 keyDecoder =
-  Decode.map toKey (Decode.field "key" Decode.string)
+    Decode.map toKey (Decode.field "key" Decode.string)
+
 
 toKey : String -> Msg
 toKey string =
-  case String.uncons string of
-    Just (char, "") ->
-      Character (Char.toUpper char)
-    _ ->
-      Control string
+    case String.uncons string of
+        Just ( char, "" ) ->
+            Character (Char.toUpper char)
+
+        _ ->
+            Control string
